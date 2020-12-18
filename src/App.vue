6457069f7fbb9357885a8c54e687d0ecbe5b6ee4
Camilo <jccastrop@unal.edu.co>
2020-12-17 20:05:12 -0500

    Add login

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
       <nav>
         <button v-on:click="init" v-if="is_auth">Inicio</button>
         <button v-on:click="getBalance" v-if="is_auth">Saldo</button>
-        <button v-on:click="getTransaction" v-if="is_auth">Transacción</button>
-        <button v-if="is_auth">Cerrar Sesión</button>
+        <button v-on:click="doTransaction" v-if="is_auth">Transacción</button>
+        <button v-on:click="logOut" v-if="is_auth">Cerrar Sesión</button>
       </nav>
     </div>
     <div class="main-component">
-      <router-view></router-view>
+      <router-view v-on:log-in="logIn"></router-view>
     </div>
     <div class="footer">
       <h2>Misión TIC 2022</h2>
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import vueRouter from "vue-router";
+
 export default {
   name: "App",
   components: {},
@@ -28,6 +30,28 @@ export default {
     };
   },
   methods: {
+    updateAuth: function () {
+      var self = this;
+      self.is_auth = localStorage.getItem("isAuth") || false;
+      if (self.is_auth == false) self.$router.push({ name: "user_auth" });
+      else {
+        let username = localStorage.getItem("current_username");
+        self.$router.push({ name: "user", params: { usuario: username } });
+      }
+    },
+
+    logIn: function (username) {
+      localStorage.setItem("current_username", username);
+      localStorage.setItem("isAuth", true);
+      this.updateAuth();
+    },
+
+    logOut: function () {
+      localStorage.removeItem("isAuth");
+      localStorage.removeItem("current_username");
+      this.updateAuth();
+    },
+
     init: function () {
       if (this.$route.name != "user") {
         let username = localStorage.getItem("current_username");
@@ -37,20 +61,23 @@ export default {
     getBalance: function () {
       if (this.$route.name != "user_balance") {
         let username = localStorage.getItem("current_username");
-        this.$router.push({name: "user_balance", params: { username: username } });
+        this.$router.push({
+          name: "user_balance",
+          params: { username: username },
+        });
       }
     },
-    getTransaction: function() {
-      if (this.$route.name != "transaction") {
-        this.$router.push({name: "transaction"});
-      }
-    }
+    doTransaction: function () {
+      let username = localStorage.getItem("current_username");
+      this.$router.push({
+        name: "user_transaction",
+        params: { username: username },
+      });
+    },
   },
-  beforeCreate: function () {
-    localStorage.setItem("current_username", "juan26");
-    localStorage.setItem("isAuth", true);
-
-    this.$router.push({name:"user",params:{usuario:'juan26'}})
+  created: function () {
+    this.$router.push({ name: "root" });
+    this.updateAuth();
   },
 };
 </script>
--- src/router.js
+++ src/router.js
@@ -2,7 +2,8 @@ import vueRouter from 'vue-router'
 
 import User from './components/User'
 import UserBalance from './components/UserBalance'
-import Transaction from './components/Transaction'
+import UserTransaction from './components/UserTransaction'
+import UserAuth from './components/UserAuth'
 import App from './App'
 
 const router = new vueRouter({
@@ -25,10 +26,16 @@ const router = new vueRouter({
             component: UserBalance
         },
         {
-            path: '/user/transaction',
-            name: "transaction",
-            component: Transaction
+            path: '/user/transaction/:username',
+            name: "user_transaction",
+            component: UserTransaction
+        },
+        {
+            path: '/user/auth',
+            name: "user_auth",
+            component: UserAuth
         }
+        
     ]
 })
 export default router
